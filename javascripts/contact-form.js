0b25292f834884aa4f8d17b71a6701878fb677bd
vjenpauline <113340493+vjenpauline@users.noreply.github.com>
2025-06-03 07:29:39 +0400

    contact

--- javascripts/contact-form.js
+++ javascripts/contact-form.js
@@ -2,6 +2,11 @@ const API_URL = window.location.hostname === 'localhost'
   ? 'http://localhost:5000/api/contact'
   : 'https://nyaoha.onrender.com/api/contact';
 
+const name = document.getElementById('name').value;
+const email = document.getElementById('email').value;
+const phone = document.getElementById('phone').value;
+const message = document.getElementById('message').value;
+
 axios.post(API_URL, {
   name,
   email,
